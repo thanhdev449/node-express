c679e2b23ce85ee1070514cf22205dc63a56f1ba
thanhdev449 <50918645+thanhdev449@users.noreply.github.com>
2020-03-04 16:19:47 +0700

    ThanhNguyen
    add source front-end and update api

--- app/controllers/admin.controller.js
+++ app/controllers/admin.controller.js
@@ -1,12 +1,20 @@
 var adminModel = require('./../models/admin.model');
+var hash = require('password-hash');
 
 module.exports.index = async function(req,res,next){
     try {
         await adminModel.find(function(err,data){
             if(err){
-                console.log(err);
+                if (err.name = 'ValidationError') {
+                    const message = Object.values(err.errors).map(res => res.message);
+                    console.log(`${message}`.red.bold.underline);
+                    res.status(400).json({
+                        success : false,
+                        error  : message,
+                    })
+                }
             }else{
-                return res.json({
+                return res.status(200).json({
                     success : true,
                     data : data,
                     error : null
@@ -14,9 +22,39 @@ module.exports.index = async function(req,res,next){
             }
         })
     } catch (error) {
-        return res.json({
+        return res.status(500).json({
             success : false,
-            error : error.message
+            error : 'Server Error'
         })
     }
 };
+
+module.exports.doCreate = async function(req,res,next){
+    try {
+        req.body.forEach(element => {
+            element.password = hash.generate(element.password);
+        });
+        await adminModel.create(req.body,function(err,data){
+            if (err) {
+                if (err.name == 'ValidationError') {
+                    const message = Object.values(err.errors).map(res => res.message);
+                    console.log(`${message}`.red.bold.underline);
+                    res.status(400).json({
+                        success : false,
+                        error : message,
+                    });
+                }
+            } else {
+                res.status(201).json({
+                    data:data,
+                    success:true,
+                }) 
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            success : false,
+            error : 'Server Error',
+        });
+    }
+}
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var controller = require('../controllers/admin.controller');
 
 router.get('/',controller.index);
+router.post('/add',controller.doCreate);
 
 module.exports = router;
