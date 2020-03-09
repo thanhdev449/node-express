3c38330b39c5c555d9bd159cefa6bbaf60ad660d
thanhdev449 <50918645+thanhdev449@users.noreply.github.com>
2020-03-09 17:16:43 +0700

    ThanhDev
    update js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ManageAdmin } from "./manage-admin/ManageAdmin";
+
+function Welcome(props){
+  return <h1>Hello, {props.name}</h1>
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Welcome name="thanh" />
+        {/* <ManageAdmin /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ front-end/src/manage-admin/ManageAdmin.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ManageAdmin extends React.Component {
+    render() {
+        return (
+            <div className="ManageAdmin">
+                <h1>Hello admin !</h1>
+            </div>
+        );
+    }
+}
+
+export default ManageAdmin;
